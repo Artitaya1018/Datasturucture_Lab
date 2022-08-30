b04efdf67e40965a7740a987450938f18140a963
Artitaya1018 <64011018@kmitl.ac.th>
2022-08-30 23:18:46 +0700

    Update item4.py

--- chapter_4_queue/item4.py
+++ chapter_4_queue/item4.py
@@ -1,7 +1,8 @@
 class Queue() :
 
     def __init__(self,list = None) :
-        if list == None : self.items = list
+        self.items = []
+        if list != None : self.items = list
     
     def enQueue(self,i) :
         self.items.append(i)
@@ -23,25 +24,43 @@ Location = { '0': 'Res.', '1':'ClassR.', '2':'SuperM.', '3':'Home'}
 q = Queue()
 q1 = Queue()
 score = 0
+myNum = ""                  # "" คือ str == Null
+myAc_Lo = ""
+yourNum = ""
+yourAc_Lo = ""
 
 for i in input :
     myq = i.split()[0]
     yrq = i.split()[1]
-    
+
+    q.enQueue(myq)
+    q1.enQueue(yrq)
+
     if myq.split(":")[0] == yrq.split(":")[0] and myq.split(":")[1] == yrq.split(":")[1] : score += 4
     elif myq.split(":")[1] == yrq.split(":")[1] : score += 2
     elif myq.split(":")[0] == yrq.split(":")[0] : score += 1
     else : score -= 5
 
-    q.enQueue(myq)
-    q1.enQueue(yrq)
+while not q.isEmpty() and not q1.isEmpty() :
+    
+    temp1 = q.deQueue()
+    temp2 = q1.deQueue()
 
 
+    myNum = myNum + temp1 + ', '
+    yourNum = yourNum + temp2 + ', '
 
-print("My   Queue",q.items)
-print("Your Queue",q1.items)
-print("My   Activity:Location = ",activity[q.items])
-print("My   Activity:Location = ",activity[q1.items])
+    myAc_Lo = myAc_Lo + activity[temp1.split(":")[0]] + ':' + Location[temp1.split(":")[1]] + ', '
+    yourAc_Lo = yourAc_Lo + activity[temp2.split(":")[0]] + ':' + Location[temp2.split(":")[1]] + ', '
+
+print("My   Queue = "+myNum[:-2])
+print("Your Queue = "+yourNum[:-2])
+print("My   Activity:Location =",myAc_Lo[:-2])
+print("Your Activity:Location =",yourAc_Lo[:-2])
+
+if score >= 7 : print("Yes! You're my love! : Score is {0}.".format(score))                      #  สมมุติ >> " คะแนน {0} , integer {2} , string {1} ".format(score,str,int) -> {ตำแหน่ง} แด้ว
+elif score > 0 : print("Umm.. It's complicated relationship! : Score is",str(score)+".")         # ใช้ + จะไม่เว้นบรรทัด ถ้าใช้ , จะเว้น
+else : print("No! We're just friends. : Score is",str(score)+".")
 
 
 
